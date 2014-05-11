b1a21b68572eec3e25641072041ccebc5bb5e083
Steven Xu <xxu@student.unimelb.edu.au>
2014-05-11 11:33:21 +1000

    added command-line parsing

---
+++ src/simplestream/Params.java
@@ -0,0 +1,17 @@
+package simplestream;
+
+import com.beust.jcommander.Parameter;
+
+public class Params {
+    @Parameter(names = "-sport", required = false, description = "Server port")
+    public Integer sport = 6262;
+
+    @Parameter(names = "-remote", required = false, description = "Remote url")
+    public String remoteUrl = null;
+
+    @Parameter(names = "-rport", required = false, description = "Remote port")
+    public Integer rport = null;
+
+    @Parameter(names = "-rate", required = false, description = "Sleep time (ms)")
+    public Integer rate = 0;
+}
