75f12ddb2085c384f227fce04fea5d2fe504369e
admin <>
2014-05-11 12:40:14 +1000

    added server handover

--- src/simplestream/ServerThread.java
+++ src/simplestream/ServerThread.java
@@ -5,121 +5,111 @@ import org.json.JSONObject;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class ServerThread implements Runnable {
+    Socket clientSocket;
     DataInputStream input;
     DataOutputStream output;
+    Server server;
 
-    public ServerThread(DataInputStream input, DataOutputStream output, Server server) {
+    public ServerThread(Socket clientSocket, DataInputStream input, DataOutputStream output, Server server) {
+        this.clientSocket = clientSocket;
         this.input = input;
         this.output = output;
+        this.server = server;
     }
 
     public void run(){
-
-        //XXX add overloaded response upon first request
-
-//			while (started) {
-//				Socket s = ss.accept();
-//				Client c = new Client(s);
-//				System.out.println("a client connected!");
-//				new Thread(c).start();
-//				clients.add(c);
-//			}
-
-
-        JSONObject status = new JSONObject();
-        JSONObject startingstream = new JSONObject();
-        JSONObject image = new JSONObject();
-        JSONObject stoppedstream = new JSONObject();
-
-
-        String stream = "local";
-        String client = "1";
-        String rate = "no";
-        String hand = "no";
-
-        try {
-            status.put("response", "status");
-            status.put("streaming", stream);
-            status.put("clients", client);
-            status.put("ratelimiting", rate);
-            status.put("handover", hand);
-
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-
-        output.writeUTF(status.toString());
-
-        String sReadStartstream = input.readUTF();
-
-        JSONObject JSONReadStartstream = new JSONObject(sReadStartstream);
-
-
-        System.out.println("status: " + status);
-        System.out.println("sReadStartstream:  " + sReadStartstream);
-        System.out.println("JSONReadStartstream:  " + JSONReadStartstream);
-
-        if (JSONReadStartstream.getString("request").equals("startstream")) {
-            System.out.println("server receive request: startstream");
-
-            try {
-                startingstream.put("response", "startingstream");
-                output.writeUTF(startingstream.toString());
-                Thread sss = new Thread();
-                sss.start();
-
-                //System.out.println("test");
-
-                int i = 0;
-                boolean temp = true;
-                //////String sReadStopstream=input.readUTF();
-
-                while (temp) {
-
-                    image.put("add", i);
-                    image.put("response", "image");
-                    image.put("data", "IMAGEDATA");
-                    i++;
-                    output.writeUTF(image.toString());
-                    System.out.println("image" + image);
-
-                    if (new JSONObject(input.readUTF()).get("request").equals("stopstream")) {
-                        temp = false;
-                        System.out.println("server receive stopstream");
-                        stoppedstream.put("response", "stoppedstream");
-                        output.writeUTF(stoppedstream.toString());
-                        System.out.println("server send stoppedstream ");
-                        listenSocket.close();
-                        clientSocket.close();
-
-                        System.out.println("server TCP close");
-
-                    }//if
-
-
-                }//while
-
-
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+        // add overloaded response upon first request
+        try{
+            String startStreamReqStr = input.readUTF();
+            try{
+                JSONObject startStreamReq = new JSONObject(startStreamReqStr);
+                if (!startStreamReq.getString("request").equals("startstream")) {
+                    System.out.println("Server didn't receive startstream request");
+                    return;
+                }
+            }catch(JSONException e){
+                System.out.println("Invalid JSON response");
+                return;
             }
-        } else {
-            System.out.println("server didn't receive startstream request");
+            if(server.isOverloaded()){
+                JSONObject status = new JSONObject();
+                try{
+                    status.put("response", "overloaded");
+                }catch(JSONException e){
+                    assert false;
+                }
+                output.writeUTF(status.toString());
+                output.flush();
+                clientSocket.close();
+                return;
+            }
+
+            JSONObject startingstream = new JSONObject();
+            JSONObject image = new JSONObject();
+            JSONObject stoppedstream = new JSONObject();
+
+            JSONObject JSONReadStartstream = new JSONObject(sReadStartstream);
+
+
+            System.out.println("status: " + status);
+            System.out.println("sReadStartstream:  " + sReadStartstream);
+            System.out.println("JSONReadStartstream:  " + JSONReadStartstream);
+
+            if (JSONReadStartstream.getString("request").equals("startstream")) {
+                System.out.println("server receive request: startstream");
+
+                try {
+                    startingstream.put("response", "startingstream");
+                    output.writeUTF(startingstream.toString());
+                    Thread sss = new Thread();
+                    sss.start();
+
+                    //System.out.println("test");
+
+                    int i = 0;
+                    boolean temp = true;
+                    //////String sReadStopstream=input.readUTF();
+
+                    while (temp) {
+
+                        image.put("add", i);
+                        image.put("response", "image");
+                        image.put("data", "IMAGEDATA");
+                        i++;
+                        output.writeUTF(image.toString());
+                        System.out.println("image" + image);
+
+                        if (new JSONObject(input.readUTF()).get("request").equals("stopstream")) {
+                            temp = false;
+                            System.out.println("server receive stopstream");
+                            stoppedstream.put("response", "stoppedstream");
+                            output.writeUTF(stoppedstream.toString());
+                            System.out.println("server send stoppedstream ");
+                            listenSocket.close();
+                            clientSocket.close();
+
+                            System.out.println("server TCP close");
+
+                        }//if
+
+
+                    }//while
+
+
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {
+            }
+
+        } catch (IOException e){
+            System.out.println("Client exited");
+            return;
         }
-
     }
-
-    catch(
-    IOException e
-    )
-
-    {
-        System.out.println("Listen socket:" + e.getMessage());
-    }
-}
 }
