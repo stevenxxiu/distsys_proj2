ac4fedfac52c5611a1f651d67b682e9d47af9c2e
Steven Xu <xxu@student.unimelb.edu.au>
2014-05-11 11:47:12 +1000

    added barney's Client.java and Server.java

--- src/simplestream/Server.java
+++ src/simplestream/Server.java
@@ -1,21 +1,125 @@
 package simplestream;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.ParameterException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-    public static void main(String[] argv) {
-        Params params = new Params();
-        try{
-            new JCommander(params, argv);
-            if(params.remoteUrl==null && params.rport!=null){
-                throw new ParameterException("-rport specified but -remoteUrl unspecified");
+    public static void main(String[] args) throws JSONException {
+
+//	boolean started = false;
+//	List<Client> clients = new ArrayList<Client>();
+//	ServerSocket ss = null;
+        try {
+            int serverPort = 6262;
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+            Socket clientSocket = listenSocket.accept();
+            System.out.println("Connection Established");
+            DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+
+//			while (started) {
+//				Socket s = ss.accept();
+//				Client c = new Client(s);
+//				System.out.println("a client connected!");			
+//				new Thread(c).start();
+//				clients.add(c);			
+//			}
+
+
+            JSONObject status = new JSONObject();
+            JSONObject startingstream = new JSONObject();
+            JSONObject image = new JSONObject();
+            JSONObject stoppedstream = new JSONObject();
+
+
+            String stream = "local";
+            String client = "1";
+            String rate = "no";
+            String hand = "no";
+
+            try {
+                status.put("response", "status");
+                status.put("streaming", stream);
+                status.put("clients", client);
+                status.put("ratelimiting", rate);
+                status.put("handover", hand);
+
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-        }catch(ParameterException e){
-            System.out.println(e);
-            System.exit(-1);
+
+
+            output.writeUTF(status.toString());
+
+            String sReadStartstream = input.readUTF();
+
+            JSONObject JSONReadStartstream = new JSONObject(sReadStartstream);
+
+
+            System.out.println("status: " + status);
+            System.out.println("sReadStartstream:  " + sReadStartstream);
+            System.out.println("JSONReadStartstream:  " + JSONReadStartstream);
+
+            if (JSONReadStartstream.getString("request").equals("startstream")) {
+                System.out.println("server receive request: startstream");
+
+                try {
+                    startingstream.put("response", "startingstream");
+                    output.writeUTF(startingstream.toString());
+                    Thread sss = new Thread();
+                    sss.start();
+
+                    //System.out.println("test");
+
+                    int i = 0;
+                    boolean temp = true;
+                    //////String sReadStopstream=input.readUTF();
+
+                    while (temp) {
+
+                        image.put("add", i);
+                        image.put("response", "image");
+                        image.put("data", "IMAGEDATA");
+                        i++;
+                        output.writeUTF(image.toString());
+                        System.out.println("image" + image);
+
+                        if (new JSONObject(input.readUTF()).get("request").equals("stopstream")) {
+                            temp = false;
+                            System.out.println("server receive stopstream");
+                            stoppedstream.put("response", "stoppedstream");
+                            output.writeUTF(stoppedstream.toString());
+                            System.out.println("server send stoppedstream ");
+                            listenSocket.close();
+                            clientSocket.close();
+
+                            System.out.println("server TCP close");
+
+                        }//if
+
+
+                    }//while
+
+
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("server didn't receive startstream request");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Listen socket:" + e.getMessage());
         }
-        boolean isLocal = (params.remoteUrl==null);
+
 
     }
 }
---
+++ src/simplestream/ServerMain.java
@@ -0,0 +1,22 @@
+package simplestream;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+public class ServerMain {
+    public static void main(String[] argv) {
+        Params params = new Params();
+        try{
+            new JCommander(params, argv);
+            if(params.remoteUrl==null && params.rport!=null){
+                throw new ParameterException("-rport specified but -remoteUrl unspecified");
+            }
+        }catch(ParameterException e){
+            System.out.println(e);
+            System.exit(-1);
+        }
+        boolean isLocal = (params.remoteUrl==null);
+
+    }
+
+}
