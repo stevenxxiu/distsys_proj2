02987fff1a342d7f3efb39641b9b414b578345e3
admin <>
2014-05-11 11:33:21 +1000

    added command-line parsing

---
+++ src/simplestream/Server.java
@@ -0,0 +1,20 @@
+package simplestream;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+public class Server {
+    public static void main(String[] argv) {
+        Params params = new Params();
+        try{
+            new JCommander(params, argv);
+            if(params.remoteUrl==null && params.rport!=null){
+                throw new ParameterException("-rport specified but -remoteUrl unspecified");
+            }
+        }catch(ParameterException e){
+            System.out.println(e);
+            System.exit(-1);
+        }
+
+    }
+}
