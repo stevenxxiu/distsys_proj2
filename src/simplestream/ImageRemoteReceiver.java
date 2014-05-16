dd9309134c4fe2a452ce1ef427cd5ee6ffda9909
zhenghao35791 <zhenghao35791@gmail.com>
2014-05-16 22:30:13 +1000

    send and receive jsonstream is working well. image can be compress and sent by String(byte[]). as well as decompress. there is a exception "GZIP problem" i havent fix it.

--- src/simplestream/ImageRemoteReceiver.java
+++ src/simplestream/ImageRemoteReceiver.java
@@ -2,6 +2,8 @@ package simplestream;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
@@ -14,11 +16,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.apache.commons.codec.binary.Base64;
+import org.bridj.Pointer;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import org.apache.commons.io.IOUtils;
+import com.github.sarxos.webcam.ds.buildin.natives.Device;
+import com.github.sarxos.webcam.ds.buildin.natives.DeviceList;
+import com.github.sarxos.webcam.ds.buildin.natives.OpenIMAJGrabber;
 
 public class ImageRemoteReceiver implements ImageReceiverInterface {
-	public Image image;
+	
 	int port;
 	String hostname;
 
@@ -29,6 +45,7 @@ public class ImageRemoteReceiver implements ImageReceiverInterface {
 
 	public void start() {
 		Socket SocketImageReceiver = null;
+
 		try {
 			SocketImageReceiver = new Socket(InetAddress.getByName(hostname),
 					port);
@@ -36,66 +53,112 @@ public class ImageRemoteReceiver implements ImageReceiverInterface {
 					.getInputStream());
 			DataOutputStream output = new DataOutputStream(SocketImageReceiver
 					.getOutputStream());
-
+			JSONObject status = new JSONObject();
 			JSONObject startstream = new JSONObject();
 			JSONObject stopstream = new JSONObject();
+			JSONObject JSONReadImageStream = new JSONObject();
+
 			try {
-//				if (Integer.valueOf(port) == null) {
-//					port = 6262;
-//				}
+				// if (Integer.valueOf(port) == null) {
+				// port = 6262;
+				// }
+				status = new JSONObject(input.readUTF());
+				System.out.println(status);
 				startstream.put("request", "startstream");
 				startstream.put("sport", port);
 
 			} catch (JSONException e) {
-				System.out.println("Invalid JSON response");
+				System.out.println("Invalid JSON response1");
 				return;
 			}
 			output.writeUTF(startstream.toString());
 			JSONObject JSONReadStartingStream = null;
+
 			try {
 				JSONReadStartingStream = new JSONObject(input.readUTF());
+				System.out.println(JSONReadStartingStream);
 			} catch (JSONException e2) {
-				System.out.println("Invalid JSON response");
+				System.out.println("Invalid JSON response2");
 				return;
 			}
 
 			try {
-				if (JSONReadStartingStream.getString("reponse").equals("overloaded")) {
+
+				if (JSONReadStartingStream.getString("response").equals(
+						"overloaded")) {
 					System.out.println("Server Overloaded, TCP Close");
 					SocketImageReceiver.close();
 				}
 			} catch (JSONException e1) {
-				System.out.println("Invalid JSON response");
+				System.out.println("Invalid JSON response3");
 				return;
 			}
 
+			while (true) {
+				try {
+					JSONReadImageStream = new JSONObject(input.readUTF());
+				} catch (JSONException e2) {
+					System.out.println("Invalid JSON response5");
+					return;
+				}
+
+				try {
+					if (JSONReadImageStream.getString("response").equals(
+							"image")) {
+						
+/*****************************************************************************************************************************/
+
+						
+						
+					/**********************************************************************************************/
+						
+						byte[] b = input.readUTF().getBytes("UTF-8");
+						System.out.println(b);
+						
+						Viewer myViewer = new Viewer();
+						JFrame frame = new JFrame("Simple Stream Viewer");
+						frame.setSize(320, 240);
+						frame.setVisible(true);
+						frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+						
+						frame.add(myViewer);
+						//myViewer.ViewerInput(b);
+						frame.repaint();
+						byte[] c = Base64.decodeBase64(b);
+						byte[] image = Compressor.decompress(c);
+						BufferedImage img = ImageIO
+								.read(new ByteArrayInputStream(image));
+						System.out.println(img);
+						// image = viewer.ViewerInput(image2);
+						// g.drawImage(image, 0, 0, null);
+					} else {
+						SocketImageReceiver.close();
+						System.out.println("client TCP close");
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			}
+
 			Scanner keyboard = new Scanner(System.in);
-			System.out.println("Input 'stop' to close the connection");
+			System.out.println("Input 'Enter' to close the connection");
+
 			String order = keyboard.next();
-			if (order.equals("stop")) {
+			System.out.println(order.length());
+
+			if (order.length() == 0) {
 				try {
 					System.out.println("Client Stop");
 					stopstream.put("request", "stopstream");
 				} catch (JSONException e) {
-					System.out.println("Invalid JSON response");
+					System.out.println("Invalid JSON response4");
 					return;
 				}
 				output.writeUTF(stopstream.toString());
 				System.out.println("stop and send request stopstream");
 			}
-			Graphics g
-			while (true) {
-				if (new JSONObject(input.readUTF()).getString("response").equals("image")) {
-					image = Compressor.decompress(Base64
-							.decode(input.readUTF()));
-					 g.drawImage(image, 0, 0, null);
-				} else {
-					SocketImageReceiver.close();
-					System.out.println("client TCP close");
-				}
-				break;
-			}
-
 		} catch (UnknownHostException e) {
 			System.out.println("Socket:" + e.getMessage());
 		} catch (EOFException e) {
@@ -111,4 +174,13 @@ public class ImageRemoteReceiver implements ImageReceiverInterface {
 				}
 		}
 	}
-};
+
+	public static void main(String[] args) throws UnknownHostException,
+			IOException {
+		Socket s = null;
+		ImageRemoteReceiver client = new ImageRemoteReceiver(6262, "localhost");
+		// s = new Socket("localhost",6262);
+		client.start();
+
+	}
+}
--- src/simplestream/ServerThread.java
+++ src/simplestream/ServerThread.java
@@ -1,96 +1,175 @@
 package simplestream;
 
+import org.apache.commons.codec.binary.Base64;
+import org.bridj.Pointer;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.Socket;
+import javax.swing.JFrame;
+
+import org.apache.commons.codec.binary.Base64;
+import org.bridj.Pointer;
+
+import com.github.sarxos.webcam.ds.buildin.natives.Device;
+import com.github.sarxos.webcam.ds.buildin.natives.DeviceList;
+import com.github.sarxos.webcam.ds.buildin.natives.OpenIMAJGrabber;
 
 public class ServerThread implements Runnable {
-    int rateLimit;
-    Socket clientSocket;
-    DataInputStream input;
-    DataOutputStream output;
-    Server server;
+	int rateLimit;
+	Socket clientSocket;
+	DataInputStream input;
+	DataOutputStream output;
+	Server server;
 
-    public ServerThread(Socket clientSocket, DataInputStream input, DataOutputStream output, int rateLimit, Server server) {
-        this.clientSocket = clientSocket;
-        this.input = input;
-        this.output = output;
-        this.rateLimit = rateLimit;
-        this.server = server;
-    }
+	public ServerThread(Socket clientSocket, DataInputStream input,
+			DataOutputStream output, int rateLimit, Server server) {
+		this.clientSocket = clientSocket;
+		this.input = input;
+		this.output = output;
+		this.rateLimit = rateLimit;
+		this.server = server;
+	}
 
-    public void run(){
-        JSONObject response;
-        // add overloaded response upon first request
-        try{
-            String requestStr = input.readUTF();
-            try{
-                JSONObject request = new JSONObject(requestStr);
-                if (!request.getString("request").equals("startstream")) {
-                    System.out.println("Server didn't receive startstream request");
-                    return;
-                }
-            }catch(JSONException e){
-                System.out.println("Invalid JSON response");
-                return;
-            }
-            if(server.isOverloaded()){
-                response = new JSONObject();
-                try{
-                    response.put("response", "overloaded");
-                }catch(JSONException e){
-                    assert false;
-                }
-                output.writeUTF(response.toString());
-                output.flush();
-                clientSocket.close();
-                return;
-            }
-            response = new JSONObject();
-            try{
-                response.put("response", "startingstream");
-            }catch(JSONException e){
-                assert false;
-            }
-            output.writeUTF(response.toString());
-            output.flush();
-            // send images to client
-            while(true){
-                try{
-                    JSONObject request = new JSONObject(requestStr);
-                    if (request.getString("request").equals("stopstream")) {
-                        response = new JSONObject();
-                        try{
-                            response.put("response", "stoppedstream");
-                        }catch(JSONException e){
-                            assert false;
-                        }
-                        break;
-                    }else if(request.getString("request").equals("ratelimit")){
-                        rateLimit = request.getInt("ratelimit");
-                    }else{
-                        System.out.println("Unknown request: " + request.getString("request"));
-                    }
-                }catch(JSONException e){
-                    System.out.println("Invalid JSON response");
-                    return;
-                }
-                // XXX send image to client
+	public void run() {
+		JSONObject response;
+		JSONObject jsonObj;
+		// add overloaded response upon first request
+		System.out.println("serverthread start");
+		try {
+			String requestStr = input.readUTF();
+			System.out.println("requestStr " + requestStr);
+			try {
+				JSONObject request = new JSONObject(requestStr);
 
-                try{
-                    Thread.sleep(rateLimit);
-                }catch(InterruptedException e){
-                    break;
-                }
-            }
-            clientSocket.close();
-        } catch (IOException e){
-            System.out.println("Client exited");
-            return;
-        }
-    }
+				if (!request.getString("request").equals("startstream")) {
+					System.out
+							.println("Server didn't receive startstream request");
+					return;
+				}
+			} catch (JSONException e) {
+				System.out.println("Invalid JSON response");
+				return;
+			}
+			if (server.isOverloaded()) {
+				response = new JSONObject();
+				try {
+					response.put("response", "overloaded");
+				} catch (JSONException e) {
+					assert false;
+				}
+				output.writeUTF(response.toString());
+				output.flush();
+				clientSocket.close();
+				return;
+			}
+			response = new JSONObject();
+			try {
+				response.put("response", "startingstream");
+			} catch (JSONException e) {
+				assert false;
+			}
+			System.out.println("response " + response);
+			output.writeUTF(response.toString());
+			output.flush();
+			// send images to client
+			while (true) {
+				try {
+					JSONObject request = new JSONObject(requestStr);
+					if (request.getString("request").equals("stopstream")) {
+						response = new JSONObject();
+						try {
+							response.put("response", "stoppedstream");
+							break;
+						} catch (JSONException e) {
+							assert false;
+						}
+						break;
+					} else if (request.getString("request").equals("ratelimit")) {
+						rateLimit = request.getInt("ratelimit");
+					} else {
+						
+/*****************************************************************************************************************************/
+
+						
+						Viewer myViewer = new Viewer();
+						JFrame frame = new JFrame("Simple Stream Viewer");
+						frame.setVisible(true);
+						frame.setSize(320, 240);
+						frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+						
+						frame.add(myViewer);
+						/**
+						 * This example show how to use native OpenIMAJ API to capture raw bytes
+						 * data as byte[] array. It also calculates current FPS.
+						 */
+
+						OpenIMAJGrabber grabber = new OpenIMAJGrabber();
+
+						Device device = null;
+						Pointer<DeviceList> devices = grabber.getVideoDevices();
+						for (Device d : devices.get().asArrayList()) {
+							device = d;
+							break;
+						}
+
+						boolean started = grabber.startSession(320, 240, 30, Pointer.pointerTo(device));
+						if (!started) {
+							throw new RuntimeException("Not able to start native grabber!");
+						}
+
+						int n = 1000;
+						int i = 0;
+						do {
+							/* Get a frame from the webcam. */
+							grabber.nextFrame();
+							/* Get the raw bytes of the frame. */
+							byte[] raw_image=grabber.getImage().getBytes(320 * 240 * 3);
+							/* Apply a crude kind of image compression. */
+							byte[] compressed_image = Compressor.compress(raw_image);
+							/* Prepare the date to be sent in a text friendly format. */
+							byte[] base64_image = Base64.encodeBase64(compressed_image);
+							/*
+							 * The image data can be sent to connected clients.
+							 */
+							/*
+							 * Assume we received some image data.
+							 * Remove the text friendly encoding.
+							 */
+							
+							
+							response = new JSONObject();
+							jsonObj = new JSONObject();
+							jsonObj.put("response", "image");
+							jsonObj.put("data",base64_image.toString());
+							System.out.println(jsonObj);
+							output.writeUTF(jsonObj.toString());
+						} while (++i < n);
+						
+						grabber.stopSession();
+					/**********************************************************************************************/
+						try {
+							Thread.sleep(rateLimit);
+						} catch (InterruptedException e) {
+							break;
+						}
+					}
+				} catch (JSONException e) {
+					System.out.println("Invalid JSON response");
+					return;
+				}
+				// XXX send image to client
+
+				
+			}
+			clientSocket.close();
+		} catch (IOException e) {
+			System.out.println("Client exited");
+			return;
+		}
+	}
 }
