93e0ffc535755d662ad89583194698f127b39f5e
Steven Xu <xxu@student.unimelb.edu.au>
2014-05-19 03:55:21 +1000

    added ClientView skeleton

---
+++ src/simplestream/ClientView.java
@@ -0,0 +1,13 @@
+package simplestream;
+
+import com.github.sarxos.webcam.ds.buildin.natives.Device;
+import com.github.sarxos.webcam.ds.buildin.natives.DeviceList;
+import com.github.sarxos.webcam.ds.buildin.natives.OpenIMAJGrabber;
+import org.apache.commons.codec.binary.Base64;
+import org.bridj.Pointer;
+
+import javax.swing.*;
+
+public class ClientView {
+
+}
