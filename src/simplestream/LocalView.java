15459ba25a540792ea7fb9c1d7aaa651af791eae
Steven Xu <xxu@student.unimelb.edu.au>
2014-05-18 21:13:15 +1000

    reverted incorrect changes, the image-viewing should be done in other classes

--- src/simplestream/LocalView.java
+++ src/simplestream/LocalView.java
@@ -19,7 +19,6 @@ public class LocalView {
 		frame.setSize(320, 240);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		frame.add(myViewer);
 		/**
 		 * This example show how to use native OpenIMAJ API to capture raw bytes
@@ -65,10 +64,8 @@ public class LocalView {
 			/* Give the raw image bytes to the viewer. */
 			myViewer.ViewerInput(decompressed_image);
 			frame.repaint();
-			//myViewer.ViewerInput(decompressed_image);
-			//myViewer.paintComponent(grabber);
 		} while (++i < n);
-		
+
 		grabber.stopSession();
 	}
 	
--- src/simplestream/Server.java
+++ src/simplestream/Server.java
@@ -12,83 +12,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
-	public int port;
-	public int maxClients;
-	public int rateLimit;
-	public boolean isLocal;
-	List<Thread> serverThreads = new ArrayList<Thread>();
+    public int port;
+    public int maxClients;
+    public int rateLimit;
+    public boolean isLocal;
+    List<Thread> serverThreads = new ArrayList<Thread>();
 
-	public Server(int port, boolean isLocal, int rateLimit, int maxClients) {
-		this.port = port;
-		this.maxClients = maxClients;
-		this.rateLimit = rateLimit;
-		this.isLocal = isLocal;
-	}
+    public Server(int port, boolean isLocal, int rateLimit, int maxClients){
+        this.port = port;
+        this.maxClients = maxClients;
+        this.rateLimit = rateLimit;
+        this.isLocal = isLocal;
+    }
 
-	public boolean isOverloaded() {
-		// check for which threads have exited
-		List<Thread> serverThreadsAlive = new ArrayList<Thread>();
-		for (Thread thread : serverThreads) {
-			if (thread.isAlive())
-				serverThreadsAlive.add(thread);
-		}
-		serverThreads = serverThreadsAlive;
-		return serverThreads.size() > maxClients;
-	}
+    public boolean isOverloaded(){
+        // check for which threads have exited
+        List<Thread> serverThreadsAlive = new ArrayList<Thread>();
+        for(Thread thread:serverThreads){
+            if(thread.isAlive())
+                serverThreadsAlive.add(thread);
+        }
+        serverThreads = serverThreadsAlive;
+        return serverThreads.size() > maxClients;
+    }
 
-	public void start() {
-		ServerSocket listenSocket;
+    public void start(){
+        ServerSocket listenSocket;
 
-		try {
-			listenSocket = new ServerSocket(port);
-		} catch (IOException e) {
-			System.out.println("Could not listen on port " + port);
-			return;
-		}
-		System.out.println("Server Started");
-		while (true) {
-			DataInputStream input;
-			DataOutputStream output;
-			Socket clientSocket;
-			try {
-				clientSocket = listenSocket.accept();
-				input = new DataInputStream(clientSocket.getInputStream());
-				output = new DataOutputStream(clientSocket.getOutputStream());
-			} catch (IOException e) {
-				System.out.println("Client exited");
-				continue;
-			}
-			System.out.println("Client connected");
-			JSONObject status = new JSONObject();
-
-			try {
-				status.put("response", "status");
-				status.put("streaming", isLocal ? "local" : "remote");
-				status.put("clients", serverThreads.size());
-				status.put("ratelimiting", "yes");
-				status.put("handover", "yes");
-			} catch (JSONException e) {
-				assert false;
-			}
-			try {
-				output.writeUTF(status.toString());
-				output.flush();
-				// clientSocket.close();
-			} catch (IOException e) {
-				System.out.println("Client exited");
-				continue;
-			}
-			Thread serverThread = new Thread(new ServerThread(clientSocket,
-					input, output, rateLimit, this));
-			serverThreads.add(serverThread);
-			serverThread.start();
-		}
-	}
-
-	public static void main(String[] args) {
-
-		Server server = new Server(6262, true, 100, 3);
-		server.start();
-
-	}
+        try {
+            listenSocket = new ServerSocket(port);
+        }catch(IOException e){
+            System.out.println("Could not listen on port " + port);
+            return;
+        }
+        System.out.println("Server Started");
+        while(true){
+            DataInputStream input;
+            DataOutputStream output;
+            Socket clientSocket;
+            try{
+                clientSocket = listenSocket.accept();
+                input = new DataInputStream(clientSocket.getInputStream());
+                output = new DataOutputStream(clientSocket.getOutputStream());
+            }catch(IOException e){
+                System.out.println("Client exited");
+                continue;
+            }
+            System.out.println("Client connected");
+            JSONObject status = new JSONObject();
+            try{
+                status.put("response", "status");
+                status.put("streaming", isLocal?"local":"remote");
+                status.put("clients", serverThreads.size());
+                status.put("ratelimiting", "yes");
+                status.put("handover", "yes");
+            }catch(JSONException e){
+                assert false;
+            }
+            try{
+                output.writeUTF(status.toString());
+                output.flush();
+                clientSocket.close();
+            }catch(IOException e){
+                System.out.println("Client exited");
+                continue;
+            }
+            Thread serverThread = new Thread(new ServerThread(clientSocket, input, output, rateLimit, this));
+            serverThreads.add(serverThread);
+            serverThread.start();
+        }
+    }
 }
