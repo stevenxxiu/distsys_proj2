356b44c49f6f35553e6fd90dcef2fa9f95ce36e7
Steven Xu <xxu@student.unimelb.edu.au>
2014-05-19 20:20:49 +1000

    call ClientView.start() in main

--- src/simplestream/ServerMain.java
+++ src/simplestream/ServerMain.java
@@ -16,12 +16,14 @@ public class ServerMain {
             System.exit(-1);
         }
         boolean isLocal = (params.remoteUrl == null);
+        int width = 320, height = 240, fps = 30;
         ImageReceiverInterface receiver;
         if(isLocal){
-            receiver = new ImageLocalReceiver();
+            receiver = new ImageLocalReceiver(width, height, fps);
         }else{
             receiver = new ImageRemoteReceiver(params.sport, params.rport, params.remoteUrl, params.rateLimit, 1000);
         }
+        new ClientView(width, height, receiver).start();
         new Server(params.sport, receiver, isLocal, params.rateLimit, 3).start();
     }
 }
